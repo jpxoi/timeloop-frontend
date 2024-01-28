75c68e773fe9ffd6266585074e3b5867998507ae
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-28 16:55:13 +0000

    Update background color and text color in App component

--- src/Components/Pages/App.jsx
+++ src/Components/Pages/App.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-3xl font-bold underline">
+    <div className="flex flex-col items-center justify-center bg-slate-800 min-h-screen py-2">
+      <h1 className="text-3xl font-bold text-white">
         Hello world!
       </h1>
     </div>
