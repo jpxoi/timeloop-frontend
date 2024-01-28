d0ed4d38cdf13917cb05da897bbf3f8782a59daf
Jean Paul Fernandez <jpxoi@icloud.com>
2024-01-28 14:19:52 +0000

    Refactor App component to use a flex container

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import './App.css'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
   )
 }
 
