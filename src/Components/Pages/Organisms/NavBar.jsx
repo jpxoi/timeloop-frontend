555884239decabe2eea1d77ccf44476286d5a6e9
d36277sm <sameer.mehmood@student.manchester.ac.uk>
2024-02-13 23:50:33 +0000

    Navbar added month, year.

--- src/Components/Pages/Organisms/NavBar.jsx
+++ src/Components/Pages/Organisms/NavBar.jsx
@@ -4,20 +4,24 @@ import NavbarProfile from '../Atoms/NavbarProfile';
 import Button from '../Atoms/Button';
 import LeftArrow from '../Atoms/LeftArrow';
 import RightArrow from '../Atoms/RightArrow';
+import DisplayMonthYear from '../Molecules/DisplayMonthYear';
 
 export default function NavBar(){
   return (
     <div className="px-4 flex items-center justify-between border-b">
-      <div className="flex items-center"> {/* Wrapper for Logo and Calendar */}
-        <Logo/>                 {/* This is the logo---Timeloop for our website*/}
+      <div className="flex items-center"> {/* Wrapper for Logo and Calendar and Today button */}
+        <Logo/>
         <h1 className='mr-10 text-lg text-blue-400'>
           Calendar
         </h1>
-        <LeftArrow buttonparams="" iconparams="w-3 h-3 mx-2"/>
-        <Button params="border rounded py-1 px-3 mx-1 text-sm border-gray-300" text="Today"/>
-        <RightArrow buttonparams="" iconparams="w-3 h-3 mx-2"/>
+        <Button params="border rounded py-2 px-3 mx-1 text-xs border-gray-300" text="Today"/>
+        <div className='flex justify-between mx-7'>
+          <LeftArrow buttonparams="" iconparams="w-3 h-3 mr-3"/>
+          <RightArrow buttonparams="" iconparams="w-3 h-3 ml-3"/>
+        </div>
+        <DisplayMonthYear params="text-black font-[400] text-[18px]"/>
       </div>
-      <div className="flex items-center"> {/* Wrapper for Today button and Profile */}
+      <div className="flex items-center"> {/* Wrapper for Profile */}
         <NavbarProfile/>
       </div>
     </div>
--- src/Components/Pages/Organisms/Sidebar.jsx
+++ src/Components/Pages/Organisms/Sidebar.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import GetMonth from "../Atoms/GetMonth";
-import GetYear from "../Atoms/GetYear";
 import SmallCalendar from "../Molecules/SmallCalendar";
+import DisplayMonthYear from "../Molecules/DisplayMonthYear";
 
 export default function Sidebar() {
     return (
       <div className="h-screen w-1/5 border pt-4 pl-[1.5%]">
-        <div className="flex">
-          <p className="text-black text-wrap text-sm font-[500] flex">
-            <span className="inline">{GetMonth()}</span> 
-            <span className="inline mx-2">{GetYear()}</span>
-          </p>
-        </div>
+        <DisplayMonthYear params="text-black font-[450] text-[13px]"/>
       </div>
     );
   }
-  
