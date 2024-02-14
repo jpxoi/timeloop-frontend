79378645cb73078327692a3e0bd546f679f17201
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-14 00:50:23 +0000

    Refactor SmallCalendar and remove SmallCalendar from Sidebar

--- src/Components/Molecules/SmallCalendar.jsx
+++ src/Components/Molecules/SmallCalendar.jsx
@@ -1,17 +1,15 @@
-import dayjs from 'dayjs';
+import dayjs from "dayjs";
 
-function SmallCalendar(month=dayjs().month(),year=dayjs().year()) {
-
+function SmallCalendar(month = dayjs().month(), year = dayjs().year()) {
   const FirstDateOfMonth = dayjs().year(year).month(month).startOf("month");
   const LastDateOfMonth = dayjs().year(year).month(month).endOf("month");
 
-  const ArrayOfDays=[];
+  const ArrayOfDays = [];
 
-  for(let i = FirstDateOfMonth.date(); i < LastDateOfMonth.date(); i++)
-  {
+  for (let i = FirstDateOfMonth.date(); i < LastDateOfMonth.date(); i++) {
     ArrayOfDays.push(FirstDateOfMonth.date(i));
   }
-  return ArrayOfDays
+  return ArrayOfDays;
 }
 
 export default SmallCalendar;
--- src/Components/Organisms/Sidebar.jsx
+++ src/Components/Organisms/Sidebar.jsx
@@ -6,7 +6,6 @@ export default function Sidebar() {
   return (
     <div className="h-screen w-1/5 border pt-4 pl-[1.5%]">
       <DisplayMonthYear params="text-black font-[450] text-[13px]" />
-      <SmallCalendar />
     </div>
   );
 }
