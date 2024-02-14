77520eb4b4b0dd5b801adda66788b88e3f18bf82
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-14 01:53:04 +0000

    Add id to sidebar component

--- src/Components/Organisms/Sidebar.jsx
+++ src/Components/Organisms/Sidebar.jsx
@@ -3,7 +3,7 @@ import DisplayMonthYear from "../Molecules/DisplayMonthYear";
 
 export default function Sidebar() {
   return (
-    <div className="h-screen w-1/5 border border-t-0 pt-4 pl-[1.5%]">
+    <div id="app-sidebar" className="h-screen w-1/5 border border-t-0 pt-4 pl-[1.5%]">
       <DisplayMonthYear params="text-black font-[450] text-[13px]" />
     </div>
   );
