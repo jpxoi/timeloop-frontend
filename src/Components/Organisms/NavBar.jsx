45530eebd2103d50ea57836ed1f4007b461be8b8
Jean Paul Fernandez <jpxoi@icloud.com>
2024-02-14 01:36:24 +0000

    Add SidebarIcon to NavBar

--- src/Components/Organisms/NavBar.jsx
+++ src/Components/Organisms/NavBar.jsx
@@ -5,13 +5,15 @@ import Button from '../Atoms/Button';
 import LeftArrow from '../Atoms/LeftArrow';
 import RightArrow from '../Atoms/RightArrow';
 import DisplayMonthYear from '../Molecules/DisplayMonthYear';
+import SidebarIcon from '../Atoms/SidebarIcon';
 
 export default function NavBar(){
   return (
     <div className="px-4 flex items-center justify-between border-b">
       <div className="flex items-center"> {/* Wrapper for Logo and Calendar and Today button */}
+        <SidebarIcon />
         <Logo/>
-        <h1 className='mr-10 text-lg text-blue-400'>
+        <h1 className='ml-2 mr-10 text-2xl text-blue-400'>
           Calendar
         </h1>
         <Button params="border rounded py-2 px-3 mx-1 text-xs border-gray-300" text="Today"/>
