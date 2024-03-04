2d088acbf4fd166db172fb834a64a5445fc05cd3
Jean Paul Fernandez <jpxoi@icloud.com>
2024-03-04 14:56:08 +0000

    Add react-router-dom and update routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/Pages/App.jsx';
+import Landing from './Components/Pages/Landing.jsx';
+import Login from './Components/Pages/Login.jsx';
+import Signup from './Components/Pages/Signup.jsx';
 import './index.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Router>
+    <Routes>
+      <Route path="/" element={<Landing />} />
+      <Route path="/app" element={<App />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  </Router>
 );
