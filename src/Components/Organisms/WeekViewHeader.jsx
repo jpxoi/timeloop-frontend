3a1f4fda7d4e42c12468722738b2f3895cf6f51f
Jean Paul Fernandez <jpxoi@icloud.com>
2024-03-07 00:08:19 +0000

    Refactor WeekViewHeader component layout

--- src/Components/Organisms/WeekViewHeader.jsx
+++ src/Components/Organisms/WeekViewHeader.jsx
@@ -5,25 +5,27 @@ const DAY_NUMBERS = [3, 4, 5, 6, 7, 8, 9];
 
 function WeekViewHeader() {
   const customStyle = {
-    paddingLeft: "65.96px",
-    display: "flex",
+    paddingLeft: "0px",
     borderBottom: "1px solid #d1d5db",
   };
 
   const dayStyle = {
-    width: "calc(100% / 7)",
     textAlign: "center",
     padding: "1rem",
   };
 
   return (
-    <div className="week-view-header" style={customStyle}>
-      {DAYS_OF_WEEK.map((day, index) => (
-        <div key={index} style={dayStyle}>
-          <div className="text-sm text-gray-500 font-normal">{day.substring(0, 3)}</div>
-          <div className="text-lg font-normal">{DAY_NUMBERS[index]}</div>
-        </div>
-      ))}
+    <div className="week-view-header grid grid-cols-[1fr_14fr]" style={customStyle}>
+      <div className="flex flex-col align-middle justify-center items-center">
+      </div>
+      <div className="grid grid-cols-7">
+        {DAYS_OF_WEEK.map((day, index) => (
+          <div className="flex flex-col align-middle justify-center items-center" key={index} style={dayStyle}>
+            <div className="text-sm text-gray-500 font-normal">{day.substring(0, 3)}</div>
+            <div className="text-lg font-normal">{DAY_NUMBERS[index]}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
