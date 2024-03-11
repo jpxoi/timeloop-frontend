a97d4c2f71ac6b37de23ec0bd4aa1a9b2039a4b5
Jean Paul Fernandez <jpxoi@icloud.com>
2024-03-11 15:43:08 +0000

    Add back to app link in AccountSettings component

--- src/Components/Pages/AccountSettings.jsx
+++ src/Components/Pages/AccountSettings.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function AccountSettings() {
   return (
     <div>
+      <Link className="text-blue-500" to="/app">Back to App</Link>
       <h1>Account Settings</h1>
     </div>
   );
