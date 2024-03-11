12e551ec68cf923041b630f9dc3811ccca45b449
n59321jh <joshua.hines@student.manchester.ac.uk>
2024-03-11 10:31:39 +0000

    Added link functionality

--- src/Components/Organisms/LandingHeader.jsx
+++ src/Components/Organisms/LandingHeader.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import Logo from "../Atoms/Logo";
 import Button from "../Atoms/Button";
+import {Link} from "react-router-dom";
 
 
 function LandingHeader() {
-    function openLink(){
-        //pass
-    }
-
     return(
         <header className="min-h-12 w-full flex items-center border-b-2 border-gray-200">
             <div className="p-1 w-fit">
                 <Logo />
             </div>
-            <Button params={"text-white bg-[#0094FF] px-4 rounded-full absolute right-2 h-7"}
-                    text={"Sign up"} 
-                    action={openLink()}/>
+            <Link to="/Signup" className="absolute right-2 h-7">
+                <Button params={"text-white bg-[#0094FF] px-4 rounded-full"}
+                        text={"Sign up"}/>
+            </Link>
         </header>
     );
 }
