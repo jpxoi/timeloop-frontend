f85841c930ea603249d74c8b7a5f5560d9309525
Jean Paul Fernandez <jpxoi@icloud.com>
2024-03-11 10:03:29 +0000

    Handle Log Out and Routing from Dropdown

--- src/Components/Atoms/DropdownMenu.jsx
+++ src/Components/Atoms/DropdownMenu.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ChevronDownIcon, ChevronUpIcon, CheckBadgeIcon } from "@heroicons/react/24/solid";
 import { ArrowRightEndOnRectangleIcon, Cog8ToothIcon, UserCircleIcon } from "@heroicons/react/24/outline";
 import Avatar from "./Avatar";
 import AvatarImg from "/src/assets/images/RndProfilePic_2_.webp";
+import { logout } from "../../Helpers/Auth";
+import { Link } from "react-router-dom";
 
 const DropdownMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,6 +12,14 @@ const DropdownMenu = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
+
+  const log_out_handler = () => {
+    logout();
+  };
+
+  useEffect(() => {
+    document.getElementById("log-out-handler").addEventListener("click", log_out_handler);
+  }, []);
 
   return (
     <div className="flex flex-col rounded-[0.5rem] bg-white relative">
@@ -38,17 +48,17 @@ const DropdownMenu = () => {
 
       <div className={`transition-all duration-[800ms] ${isOpen ? "max-h-[300px]" : "max-h-0"} overflow-hidden`}>
         <ul className="flex flex-col mt-4 text-[12px] font-[400]">
-          <li className="flex border-2 border-white rounded-[0.5rem] py-2 px-2 hover:border-gray-200 space-x-2">
+          <Link className="flex border-2 border-white rounded-[0.5rem] py-2 px-2 hover:border-gray-200 space-x-2" to="/app/profile">
             <UserCircleIcon className="w-5 h-5" />
             <div className="">Profile</div>
-          </li>
-          <li className="flex border-2 border-white rounded-[0.5rem] py-2 px-2 hover:border-gray-200 space-x-2">
+          </Link>
+          <Link className="flex border-2 border-white rounded-[0.5rem] py-2 px-2 hover:border-gray-200 space-x-2" to="/app/account">
             <Cog8ToothIcon className="w-5 h-5" />
             <div className="">Settings</div>
-          </li>
+          </Link>
           <div className="border-b border-gray-300 w-full my-2" />
-          <li className="flex border-2 border-white justify-between rounded-[0.5rem] py-2 px-2 hover:border-gray-200 text-red-600 space-x-2 font-[500]">
-            <div className="">Log out</div>
+          <li id="log-out-handler" className="flex border-2 border-white justify-between rounded-[0.5rem] py-2 px-2 hover:border-gray-200 text-red-600 space-x-2 font-[500] cursor-pointer">
+            <div className="cursor-pointer">Log out</div>
             <div><ArrowRightEndOnRectangleIcon className="w-5 h-5" /></div>
           </li>
         </ul>
