042b8c64ed706d7b24114e645d4476c222e88ab3
n59321jh <joshua.hines@student.manchester.ac.uk>
2024-03-28 21:03:50 +0000

    Sync2City selected time card added

--- src/Components/Pages/Landing.jsx
+++ src/Components/Pages/Landing.jsx
@@ -5,6 +5,7 @@ import Card1 from "/src/assets/illustrations/Card1.svg";
 import Card2 from "/src/assets/illustrations/Card2.svg";
 import GlobeSymbol from "/src/assets/illustrations/GlobeSymbol.svg";
 import WeekWrappedSymbol from "/src/assets/illustrations/WeekWrappedSymbol.svg";
+import PeopleSymbol from "/src/assets/images/People.webp";
 import {Link} from "react-router-dom";
 
 function Landing() {
@@ -28,7 +29,13 @@ function Landing() {
       <div className="min-w-max max-w-screen grid grid-cols-3 gap-10 auto-cols-max justify-evenly justify-items-stretch items-center my-28 mx-40 p-10 bg-[#F1FBFF] rounded-lg">
         <div className="h-96 bg-white rounded-lg shadow-xl min-w-40">
           <h1 className="font-semibold text-2xl p-4"> <img src={GlobeSymbol} className="inline"></img> Sync<p className="inline text-red-600">2City</p> </h1>
-          <p className="p-4 w-80">Easily view multiple time zones at a glance, enabling you to coordinate with colleagues, clients, or friends around the world.</p>
+          <p className="p-4 w-80">Easily view multiple time zones at a glance, enabling you to synchronise with colleagues, clients, or friends around the world.</p>
+
+          <div className="rounded-lg h-14 mx-2 bg-gradient-to-r from-blue-600 via-blue-300 to-blue-600 shadow-xl relative">
+            <h1 className="text-white font-semibold pt-2 pl-4">Singapore</h1>
+            <h2 className="text-white pl-4 font-light absolute bottom-1">8 hours ahead, tomorrow</h2>
+            <h1 className="text-white text-xl font-semibold absolute bottom-3 right-4">05:56</h1>
+          </div>
         </div>
 
         <div className="h-96 bg-white rounded-lg shadow-xl min-w-40">
@@ -37,7 +44,8 @@ function Landing() {
         </div>
 
         <div className="h-96 bg-white rounded-lg shadow-xl min-w-40">
-  
+          <h1 className="font-semibold text-2xl p-4"> <img src={PeopleSymbol} className="inline w-8 h-8"></img> Get Social</h1>
+          <p className="p-4 w-80">Share events with other people on TimeLoop, making coordination effortless. </p>
         </div>
       </div>
     </div>
