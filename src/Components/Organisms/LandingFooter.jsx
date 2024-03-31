dd82f73bfce96e48da10991faceb31cf192b4540
Jean Paul Fernandez <jpxoi@icloud.com>
2024-03-31 19:26:13 +0100

    Refactor LandingHeader and LandingFooter components

--- src/Components/Organisms/LandingFooter.jsx
+++ src/Components/Organisms/LandingFooter.jsx
@@ -1,22 +1,36 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import Logo from "/src/assets/images/logo.webp";
 
 function LandingFooter() {
-    return (
-        <div>
-            <div className="bg-gray-100 border-t-2 border-t-gray-200 p-10 flex justify-around items-center">
-                <img src={Logo} className="w-[75px]"></img>
-                <Link className="hover:text-sky-500 hover:underline">Calendar</Link>
-                <Link className="hover:text-sky-500 hover:underline">Account</Link>
-                <Link to="/Signup" className="hover:text-sky-500 hover:underline">Sign up</Link>
-            </div>
-            <div className="bg-gray-100 pb-5 relative">
-                <p className="text-sm font-light absolute right-[70px] bottom-5"><a target="_blank" href="https://icons8.com/icon/85369/people">Community</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <div className="bg-gray-100 border-t-2 border-t-gray-200 p-10 flex justify-around items-center">
+        <img src={Logo} className="w-[75px]"></img>
+        <Link to="/app" className="hover:text-sky-500 hover:underline">
+          Calendar
+        </Link>
+        <Link to="/app/account" className="hover:text-sky-500 hover:underline">
+          Account
+        </Link>
+        <Link to="/Signup" className="hover:text-sky-500 hover:underline">
+          Sign up
+        </Link>
+      </div>
+      <div className="bg-gray-100 pb-5 relative">
+        <p className="text-sm font-light absolute right-[70px] bottom-5">
+          <a target="_blank" rel="noreferrer" href="https://icons8.com/icon/85369/people">
+            Community
+          </a>{" "}
+          icon by{" "}
+          <a target="_blank" rel="noreferrer" href="https://icons8.com">
+            Icons8
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default LandingFooter;
--- src/Components/Organisms/LandingHeader.jsx
+++ src/Components/Organisms/LandingHeader.jsx
@@ -1,28 +1,31 @@
 import React from "react";
 import Logo from "../Atoms/Logo";
 import Button from "../Atoms/Button";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
 
 function LandingHeader() {
-    return(
-        <header className="min-h-12 w-full flex items-center border-b-2 border-gray-200">
-            <div className="p-1 w-fit">
-                <Link to="/">
-                    <Logo />
-                </Link>
-            </div>
-            <Link to="/Signup" className="absolute right-2">
-                <Button params={"text-white bg-[#0094FF] px-4 rounded-full min-h-8"}
-                        text={"Sign up"}/>
-            </Link>
+  return (
+    <header className="min-h-12 w-full flex items-center border-b-2 border-gray-200">
+      <div className="p-1 w-fit">
+        <Link to="/">
+          <Logo />
+        </Link>
+      </div>
+      <Link to="/Signup" className="absolute right-2">
+        <Button
+          params={"text-white bg-[#0094FF] px-4 rounded-lg min-h-8 hover:bg-[#0072E3]"}
+          text={"Sign up"}
+        />
+      </Link>
 
-            <Link to="/Login" className="absolute right-28">
-                <Button params={"text-black px-4 rounded-full min-h-8 border-2 border-black"}
-                        text={"Log in"}/>
-            </Link>
-        </header>
-    );
+      <Link to="/Login" className="absolute right-28">
+        <Button
+          params={"text-black px-4 rounded-lg min-h-8 border border-gray-70 hover:bg-gray-200"}
+          text={"Log in"}
+        />
+      </Link>
+    </header>
+  );
 }
 
 export default LandingHeader;
