55c31cd2c314e19b9c60284c77dddc177590c24d
d36277sm <sameer.mehmood@student.manchester.ac.uk>
2024-04-01 01:12:36 +0100

    Sidebar styling

--- src/Components/Atoms/Weather.jsx
+++ src/Components/Atoms/Weather.jsx
@@ -1,22 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import {WEATHER_API_URL, WEATHER_API_KEY} from "../../../env";
+import { WEATHER_API_URL, WEATHER_API_KEY } from "../../../env";
+import WindSvg from './WindWeather';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 const Weather = () => {
+    const [showInput, setShowInput] = useState(false);
     const [weatherData, setWeatherData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
     const [city, setCity] = useState('');
-    
+
     useEffect(() => {
-        fetchWeatherData(city);
-    }, [city]);
-    
-    const fetchWeatherData = async (city) => {
+        // Fetch the device's default location
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const { latitude, longitude } = position.coords;
+                fetchWeatherData(latitude, longitude);
+            },
+            (error) => {
+                console.error('Error getting location:', error);
+                // Handle error, for example, by setting default coordinates
+                fetchWeatherData(DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
+            }
+        );
+    }, []);
+
+    const handleSearchIconClick = () => {
+        setShowInput(true);
+    };
+
+    const fetchWeatherData = async (latitude, longitude) => {
         try {
             setLoading(true);
             setError(null);
-            // Fetch weather data using city name
-            const response = await fetch(`${WEATHER_API_URL}?q=${city}&units=metric&appid=${WEATHER_API_KEY}`);
+            // Fetch weather data using coordinates
+            const response = await fetch(`${WEATHER_API_URL}?lat=${latitude}&lon=${longitude}&units=metric&appid=${WEATHER_API_KEY}`);
             if (!response.ok) {
                 throw new Error('Failed to fetch weather data');
             }
@@ -34,25 +52,36 @@ const Weather = () => {
     };
 
     return (
-        <div className=''>
-            <input 
-                type="text" 
-                placeholder="Enter city name" 
-                value={city} 
-                onChange={handleCityChange} 
-                className='h-[1.5rem] w-[12rem]'
-            />
+        <div className='bg-white p-3 border rounded-[0.5rem]'>
+            <div className="flex items-center ">
+                <MagnifyingGlassIcon className="h-4 w-4 cursor-pointer" onClick={handleSearchIconClick} />
+                {showInput && (
+                    <form className='flex items-center'>
+                        <input
+                            type="text"
+                            placeholder="Enter city name"
+                            value={city}
+                            onChange={handleCityChange}
+                            className='h-[1.5rem] w-[12rem] mr-2'
+                        />
+                        <button type="button" onClick={() => setShowInput(false)} className="bg-blue-500 text-white px-2 py-1 rounded">Close</button>
+                    </form>
+                )}
+            </div>
             {loading && <p>Loading...</p>}
             {error && <p className='text-[0.8rem] text-red-500'>{error}</p>}
             {weatherData && (
-                <div className='text-[0.8rem] flex flex-col border-2 p-3 rounded-[0.5rem]'>
+                <div className='text-[0.8rem] flex flex-col'>
                     <div className='flex justify-between items-center text-[1rem] font-[400]'>
-                        <h2 >{weatherData.name}, {weatherData.sys.country}</h2> 
+                        <h2 className='font-[550] text-[1.1rem]'>{weatherData.name}, {weatherData.sys.country}</h2>
                         <p className='text-[1.4rem]'>{weatherData.main.temp} °C</p>
                     </div>
                     <p>Weather: {weatherData.weather[0].description}</p>
                     <p>Humidity: {weatherData.main.humidity}%</p>
-                    <p>Wind Speed: {weatherData.wind.speed} m/s</p>
+                    <div className='flex space-x-2'>
+                        <WindSvg />
+                        <p>{weatherData.wind.speed} m/s</p>
+                    </div>
                 </div>
             )}
         </div>
@@ -60,6 +89,8 @@ const Weather = () => {
 };
 
 export default Weather;
+
+
 
 
 
---
+++ src/Components/Molecules/MiniCalendarDropdown.jsx
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+import {
+  CalendarDaysIcon,
+  ChevronRightIcon,
+  ChevronDownIcon,
+} from "@heroicons/react/24/outline";
+import LeftArrow from "../Atoms/LeftArrow";
+import RightArrow from "../Atoms/RightArrow";
+import { MiniCalendar } from "../Molecules/MiniCalendar";
+import DisplayMonthYear from "../Molecules/DisplayMonthYear";
+import cn from "classnames";
+import dayjs from "dayjs";
+
+
+export default function CalendarComponent({ currentMonth, setCurrentMonth }) {
+
+  const [showCalendar, setShowCalendar] = useState(false);
+  const day = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
+
+
+  const toggleCalendar = () => {
+    setShowCalendar((prevState) => !prevState);
+  };
+
+  const GetNextMonth = () => {
+    const newMonth = dayjs(currentMonth, "MMMM YYYY")
+      .add(1, "month")
+      .format("MMMM YYYY");
+    setCurrentMonth(newMonth);
+  };
+
+  const GetPreviousMonth = () => {
+    const newMonth = dayjs(currentMonth, "MMMM YYYY")
+      .subtract(1, "month")
+      .format("MMMM YYYY");
+    setCurrentMonth(newMonth);
+  };
+
+  return (
+    <div className="flex flex-col p-2 rounded-[0.5rem] text-gray-700 bg-white relative border">
+      <div
+        className="flex space-x-3 items-center  text-[0.9rem] font-[500] cursor-pointer"
+        onClick={toggleCalendar}
+      >
+        <button className="flex items-center ">
+          {showCalendar ? (
+            <ChevronDownIcon className="h-4 w-4 font-[600]" />
+          ) : (
+            <ChevronRightIcon className="h-4 w-4 font-[600] " />
+          )}
+        </button>
+
+        <CalendarDaysIcon className="w-5 h-5 text-gray-700 cursor-pointer " />
+        <div>Calendar</div>
+      </div>
+
+      <div
+        className={`transition-all duration-[500ms] ${
+          showCalendar ? "max-h-[500px]" : "max-h-0"
+        } overflow-hidden`}
+      >
+        <div className="rounded-[0.5rem] pt-2 items-center">
+          <div className="flex flex-col text-gray-800 rounded-[0.5rem]">
+            <div className="flex justify-between items-center pl-2 py-2">
+              <div>
+                <DisplayMonthYear
+                  params="font-[600] text-[1rem]"
+                  date={currentMonth}
+                />
+              </div>
+
+              <div className="items-center">
+                <LeftArrow
+                  buttonparams=""
+                  iconparams="w-4 h-4"
+                  action={GetPreviousMonth}
+                />
+
+                <RightArrow
+                  buttonparams=""
+                  iconparams="w-4 h-4"
+                  action={GetNextMonth}
+                  className={`transition-all duration-[800ms] `}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-7 mt-1 rounded-[0.5rem] py-1 mb-2">
+            {day.map((day, index) => {
+              let textColorClass = "text-blue-500";
+              if (day === "SU" || day === "SA") {
+                textColorClass = "text-rose-500";
+              }
+
+              return (
+                <div key={index}>
+                  <h1
+                    className={`text-[0.6rem] font-[500] grid place-content-center ${textColorClass}`}
+                  >
+                    {day}
+                  </h1>
+                </div>
+              );
+            })}
+          </div>
+
+          <div className="h-[155px] grid grid-cols-7 mt-1">
+            {MiniCalendar({
+              month: dayjs(currentMonth).month(),
+              year: dayjs(currentMonth).year(),
+            }).map(({ date, currentMonth, today }, index) => {
+              return (
+                <div
+                  key={index}
+                  className="text-[0.5rem] font-[600] grid place-content-center"
+                >
+                  <button
+                    className={cn(
+                      currentMonth
+                        ? "font-[400] text-[0.6rem]"
+                        : "text-gray-400 font-[400]",
+                      today ? "bg-blue-600 text-white font-bold" : "",
+                      "h-5 w-5 grid place-content-center rounded hover:border-blue-600 hover:border-2 hover:bg-white hover:text-black transition-all cursor-pointer"
+                    )}
+                  >
+                    {date.date()}
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
